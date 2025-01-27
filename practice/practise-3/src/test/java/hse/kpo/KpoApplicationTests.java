5603eb8b3a487b495c673890aa389c42f67c423a
pipitochka <aaakulov_3@edu.hse.ru>
2025-01-27 19:20:02 +0300

    finish work

--- practice/practise-3/src/test/java/hse/kpo/KpoApplicationTests.java
+++ practice/practise-3/src/test/java/hse/kpo/KpoApplicationTests.java
@@ -8,7 +8,6 @@ import hse.kpo.params.PedalEngineParams;
 import hse.kpo.services.CarService;
 import hse.kpo.services.CustomerStorage;
 import hse.kpo.services.HseCarService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,19 +34,11 @@ class KpoApplicationTests {
 	@Test
 	@DisplayName("Тест загрузки контекста")
 	void contextLoads() {
-		Assertions.assertNotNull(carService);
-		Assertions.assertNotNull(customerStorage);
-		Assertions.assertNotNull(hseCarService);
-	}
-
-	@Test
-	@DisplayName("Тест загрузки контекста")
-	void hseCarServiceTest() {
 		customerStorage.addCustomer(new Customer("Ivan1",6,4));
 		customerStorage.addCustomer(new Customer("Maksim",4,6));
 		customerStorage.addCustomer(new Customer("Petya",6,6));
 		customerStorage.addCustomer(new Customer("Nikita",4,4));
-		
+
 		carService.addCar(pedalCarFactory, new PedalEngineParams(6));
 		carService.addCar(pedalCarFactory, new PedalEngineParams(6));
 
