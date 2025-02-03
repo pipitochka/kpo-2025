33b4de2e5b8d4c3f577ad7097a2314b6a9d8c430
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-03 17:49:34 +0300

    4 sem

--- practice/practise-4/src/main/java/hse/kpo/KpoApplication.java
+++ practice/practise-4/src/main/java/hse/kpo/KpoApplication.java
@@ -1,19 +1,21 @@
 package hse.kpo;
 
+import hse.kpo.domains.Customer;
+import hse.kpo.factories.HandCarFactory;
+import hse.kpo.factories.PedalCarFactory;
+import hse.kpo.params.EmptyEngineParams;
+import hse.kpo.params.PedalEngineParams;
+import hse.kpo.services.CarService;
+import hse.kpo.services.CustomerStorage;
+import hse.kpo.services.HseCarService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * main class to entry in program.
- */
+
 @SpringBootApplication
 public class KpoApplication {
-    /**
-     * entry point of program.
-     *
-     * @param args special args.
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(KpoApplication.class, args);
-    }
+	public static void main(String[] args) {
+
+		SpringApplication.run(KpoApplication.class, args);
+	}
 }
--- practice/practise-4/src/main/java/hse/kpo/services/CustomerStorage.java
+++ practice/practise-4/src/main/java/hse/kpo/services/CustomerStorage.java
@@ -1,16 +1,14 @@
 package hse.kpo.services;
 
 import hse.kpo.domains.Customer;
-import hse.kpo.interfaces.CustomerProviderInterface;
-import java.util.ArrayList;
-import java.util.List;
+import hse.kpo.interfaces.ICustomerProvider;
 import org.springframework.stereotype.Component;
 
-/**
- * class of customer storage.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class CustomerStorage implements CustomerProviderInterface {
+public class CustomerStorage implements ICustomerProvider {
     private List<Customer> customers = new ArrayList<>();
 
     @Override
@@ -18,7 +16,8 @@ public class CustomerStorage implements CustomerProviderInterface {
         return customers;
     }
 
-    public void addCustomer(Customer customer) {
+    public void addCustomer(Customer customer)
+    {
         customers.add(customer); // просто добавляем покупателя в список
     }
 }
