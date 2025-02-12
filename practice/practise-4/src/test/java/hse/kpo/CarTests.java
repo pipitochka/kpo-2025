936f69d7062355e9960b5d79fab9ce17832afcda
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-13 00:46:03 +0300

    change textstyle

--- practice/practise-4/src/test/java/hse/kpo/CarTests.java
+++ practice/practise-4/src/test/java/hse/kpo/CarTests.java
@@ -1,36 +1,33 @@
 package hse.kpo;
 
-import hse.kpo.factories.FlyingCarFactory;
-import hse.kpo.factories.HandCarFactory;
-import hse.kpo.factories.PedalCarFactory;
+import static org.mockito.Mockito.verify;
+
+import hse.kpo.factories.FlyingCarFactoryInterface;
+import hse.kpo.factories.HandCarFactoryInterface;
+import hse.kpo.factories.PedalCarFactoryInterface;
 import hse.kpo.params.EmptyEngineParams;
-import hse.kpo.params.PedalEngineParams;
-import hse.kpo.services.HseCarService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.mockito.Mockito.verify;
-
+/**
+ * car tests.
+ */
 @SpringBootTest
 public class CarTests {
     @Autowired
-    private FlyingCarFactory flyingCarFactory;
+    private FlyingCarFactoryInterface flyingCarFactory;
 
     @Autowired
-    private HandCarFactory handCarFactory;
+    private HandCarFactoryInterface handCarFactory;
 
     @Autowired
-    private PedalCarFactory pedalCarFactory;
-
-
+    private PedalCarFactoryInterface pedalCarFactory;
 
     @Test
     @DisplayName("FlyingCarFactory")
-    void FlyingCarFactoryTest() {
+    void flyingCarFactoryTest() {
         flyingCarFactory.createCar(EmptyEngineParams.DEFAULT, 1);
         flyingCarFactory.createCar(EmptyEngineParams.DEFAULT, 2);
         flyingCarFactory.createCar(EmptyEngineParams.DEFAULT, 3);
@@ -38,21 +35,10 @@ public class CarTests {
 
     @Test
     @DisplayName("HandCarFactory")
-    void HandCarFactoryTest() {
+    void handCarFactoryTest() {
         handCarFactory.createCar(EmptyEngineParams.DEFAULT, 1);
         handCarFactory.createCar(EmptyEngineParams.DEFAULT, 2);
         handCarFactory.createCar(EmptyEngineParams.DEFAULT, 3);
     }
 
-    @Test
-    @DisplayName("HandCarFactory")
-    void PedalCarFactoryTest() {
-        var pedalParamsMock = Mockito.mock(PedalEngineParams.class);
-        Mockito.when(pedalParamsMock.pedalSize()).thenReturn(3);
-        Assertions.assertEquals(pedalCarFactory.createCar(pedalParamsMock, 1).getVIN(), 3);
-//		pedalCarFactory.createCar(pedalParamsMock, 2);
-//		pedalCarFactory.createCar(pedalParamsMock,  3);
-
-        verify(pedalParamsMock).pedalSize();
-    }
 }
