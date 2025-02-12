e1644cf7a0c6ecb9cf82c4471078cebb806d3623
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-13 00:46:03 +0300

    change textstyle

--- practice/practise-4/src/main/java/hse/kpo/services/CustomerStorage.java
+++ practice/practise-4/src/main/java/hse/kpo/services/CustomerStorage.java
@@ -1,14 +1,16 @@
 package hse.kpo.services;
 
 import hse.kpo.domains.Customer;
-import hse.kpo.interfaces.ICustomerProvider;
-import org.springframework.stereotype.Component;
-
+import hse.kpo.interfaces.CustomerProviderInterface;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Component;
 
+/**
+ * class of customer storage.
+ */
 @Component
-public class CustomerStorage implements ICustomerProvider {
+public class CustomerStorage implements CustomerProviderInterface {
     private List<Customer> customers = new ArrayList<>();
 
     @Override
@@ -16,8 +18,7 @@ public class CustomerStorage implements ICustomerProvider {
         return customers;
     }
 
-    public void addCustomer(Customer customer)
-    {
+    public void addCustomer(Customer customer) {
         customers.add(customer); // просто добавляем покупателя в список
     }
 }
