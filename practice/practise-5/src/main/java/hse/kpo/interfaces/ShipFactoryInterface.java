85a1645cd1f27de79caabb1b58b6da7a906e49f0
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-13 12:36:59 +0300

    change textstyle

--- practice/practise-5/src/main/java/hse/kpo/interfaces/ShipFactoryInterface.java
+++ practice/practise-5/src/main/java/hse/kpo/interfaces/ShipFactoryInterface.java
@@ -2,6 +2,9 @@ package hse.kpo.interfaces;
 
 import hse.kpo.domains.Ship;
 
+/**
+ * interface of ship factory.
+ */
 public interface ShipFactoryInterface<ParamsT> {
     Ship createShip(ParamsT shipParams, int shipNumber);
 }
