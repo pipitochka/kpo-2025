947749dab6639aa54473ac474c74cab68d134670
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-13 12:17:52 +0300

    add ships with test, 95% covering

---
+++ practice/practise-5/src/main/java/hse/kpo/services/CarService.java
@@ -0,0 +1,50 @@
+package hse.kpo.services;
+
+import hse.kpo.domains.Car;
+import hse.kpo.domains.Customer;
+import hse.kpo.domains.ProductionTypes;
+import hse.kpo.interfaces.CarFactoryInterface;
+import hse.kpo.interfaces.CarProviderInterface;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+/**
+ * class of car service.
+ */
+@Component
+public class CarService implements CarProviderInterface {
+
+    private final List<Car> cars = new ArrayList<>();
+
+    private int carNumberCounter = 0;
+
+    @Override
+    public Car takeCar(Customer customer) {
+
+        var filteredCars = cars.stream().filter(car -> car.isCompatible(customer)).toList();
+
+        var firstCar = filteredCars.stream().findFirst();
+
+        firstCar.ifPresent(cars::remove);
+
+        return firstCar.orElse(null);
+    }
+
+    /**
+     * function which make car and add it to the pool.
+     *
+     * @param carFactory factory which will make car.
+     * @param carParams car params which used in car constructor.
+     * @param <ParamsT> params for constructor.
+     */
+    public <ParamsT> void addCar(CarFactoryInterface<ParamsT> carFactory, ParamsT carParams) {
+        // создаем автомобиль из переданной фабрики
+        var car = carFactory.createCar(
+                carParams, // передаем параметры
+                ++carNumberCounter // передаем номер - номер будет начинаться с 1
+        );
+
+        cars.add(car); // добавляем автомобиль
+    }
+}
--- practice/practise-5/src/main/java/hse/kpo/services/CustomerStorage.java
+++ practice/practise-5/src/main/java/hse/kpo/services/CustomerStorage.java
@@ -1,24 +1,24 @@
 package hse.kpo.services;
 
 import hse.kpo.domains.Customer;
-import hse.kpo.enums.ProductionTypes;
-import hse.kpo.interfaces.ICustomerProvider;
-import org.springframework.stereotype.Component;
-
+import hse.kpo.interfaces.CustomerProviderInterface;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Component;
 
+/**
+ * class of customer storage.
+ */
 @Component
-public class CustomerStorage implements ICustomerProvider {
-    private final List<Customer> customers = new ArrayList<>();
+public class CustomerStorage implements CustomerProviderInterface {
+    private List<Customer> customers = new ArrayList<>();
 
     @Override
     public List<Customer> getCustomers() {
         return customers;
     }
 
-    public void addCustomer(Customer customer)
-    {
+    public void addCustomer(Customer customer) {
         customers.add(customer); // просто добавляем покупателя в список
     }
 }
