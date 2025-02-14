194d3c69cb97cec8adc856bc8497b2f121144a9b
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-14 12:16:57 +0300

    start homework

---
+++ miniHomeworks/miniHomeworks-1/src/main/java/zoo/Main.java
@@ -0,0 +1,19 @@
+package zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * main class to entry in program.
+ */
+@SpringBootApplication
+public class Main {
+    /**
+     * entry point of program.
+     *
+     * @param args special args.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
