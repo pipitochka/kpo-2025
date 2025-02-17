2029a210e14caddd22a18f5eb7963c635fc539c3
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-17 17:17:02 +0300

    start 6 sem

---
+++ practice/practise-6/src/main/java/hse/kpo/services/services/ShipService.java
@@ -0,0 +1,49 @@
+package hse.kpo.services;
+
+import hse.kpo.domains.Customer;
+import hse.kpo.domains.Ship;
+import hse.kpo.interfaces.ShipFactoryInterface;
+import hse.kpo.interfaces.ShipProviderInterface;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+/**
+ * class of ship service.
+ */
+@Component
+public class ShipService implements ShipProviderInterface {
+
+    private final List<Ship> ships = new ArrayList<>();
+
+    private int shipNumberCounter = 0;
+
+    @Override
+    public Ship takeShip(Customer customer) {
+
+        var filteredShips = ships.stream().filter(ship -> ship.isCompatible(customer)).toList();
+
+        var firstShip = filteredShips.stream().findFirst();
+
+        firstShip.ifPresent(ships::remove);
+
+        return firstShip.orElse(null);
+    }
+
+    /**
+     * function which make and add ship to the pool.
+     *
+     * @param shipFactory factory which will make ship.
+     * @param shipParams params for ship construcor.
+     * @param <ParamsT> params for ship construcor.
+     */
+    public <ParamsT> void addShip(ShipFactoryInterface<ParamsT> shipFactory, ParamsT shipParams) {
+        // создаем автомобиль из переданной фабрики
+        var car = shipFactory.createShip(
+                shipParams, // передаем параметры
+                ++shipNumberCounter // передаем номер - номер будет начинаться с 1
+        );
+
+        ships.add(car); // добавляем автомобиль
+    }
+}
---
+++ practice/practise-6/src/main/java/hse/kpo/services/storage/CustomerStorage.java
@@ -0,0 +1,24 @@
+package hse.kpo.services;
+
+import hse.kpo.domains.Customer;
+import hse.kpo.interfaces.CustomerProviderInterface;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+/**
+ * class of customer storage.
+ */
+@Component
+public class CustomerStorage implements CustomerProviderInterface {
+    private List<Customer> customers = new ArrayList<>();
+
+    @Override
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer); // просто добавляем покупателя в список
+    }
+}
