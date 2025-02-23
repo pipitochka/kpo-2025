b3e9c7d676944769fba92ce818f23d88d2ec4eff
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-23 13:59:48 +0300

    start preparing 7 sem

---
+++ practice/practise-7/src/main/java/hse/kpo/domains/engines/HandEngine.java
@@ -0,0 +1,27 @@
+package hse.kpo.domains.engines;
+
+import hse.kpo.domains.objects.Customer;
+import hse.kpo.enums.ProductionTypes;
+import hse.kpo.interfaces.engines.EngineInterface;
+import lombok.ToString;
+
+/**
+ * class of hand engine.
+ */
+@ToString
+public class HandEngine implements EngineInterface {
+
+    /**
+     * Проверяет подходит ли двигатель покупателю.
+     *
+     * @param customer - покупатель, с которым мы сравниваем двигатель.
+     */
+    @Override
+    public boolean isCompatible(Customer customer, ProductionTypes type) {
+        return switch (type) {
+            case hse.kpo.enums.ProductionTypes.CAR -> customer.getHandPower() > 5;
+            case hse.kpo.enums.ProductionTypes.CATAMARAN -> customer.getHandPower() > 2;
+            case null, default -> throw new RuntimeException("This type of production doesn't exist");
+        };
+    }
+}
