2d71342f12b4d175cd74d321cd272e74b750bd5a
pipitochka <aaakulov_3@edu.hse.ru>
2025-03-03 17:35:05 +0300

    add reports

--- practice/practise-8/src/main/java/hse/kpo/domains/reports/MarkdownReportExporter.java
+++ practice/practise-8/src/main/java/hse/kpo/domains/reports/MarkdownReportExporter.java
@@ -1,16 +1,15 @@
 package hse.kpo.domains.reports;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hse.kpo.interfaces.reports.ReportExporter;
 
 import java.io.IOException;
 import java.io.Writer;
 
-public class JsonReportExporter implements ReportExporter {
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
+public class MarkdownReportExporter implements ReportExporter {
     @Override
     public void export(Report report, Writer writer) throws IOException {
-        objectMapper.writeValue(writer, report);
+        writer.write("# " + report.title() + "\n\n");
+        writer.write(report.content());
+        writer.flush();
     }
 }
