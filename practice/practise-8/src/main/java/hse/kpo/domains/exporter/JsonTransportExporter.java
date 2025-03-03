43d40aab2534de8b033f20ec2dbb55010dbeb636
pipitochka <aaakulov_3@edu.hse.ru>
2025-03-03 23:37:37 +0300

    finish sem

--- practice/practise-8/src/main/java/hse/kpo/domains/exporter/JsonTransportExporter.java
+++ practice/practise-8/src/main/java/hse/kpo/domains/exporter/JsonTransportExporter.java
@@ -1,28 +1,20 @@
 package hse.kpo.domains.exporter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import hse.kpo.interfaces.transport.Transport;
 import hse.kpo.interfaces.reports.TransportExporter;
 
 import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
+import java.util.Objects;
 
 public class JsonTransportExporter implements TransportExporter {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void export(List<Transport> transports, Writer writer) throws IOException {
-        transports.forEach(transport ->
-        {
-            try {
-                objectMapper.writeValue(writer, String.format("%d,%s,%s\n",
-                        transport.getVin(),
-                        transport.getTransportType(),
-                        transport.getEngineType()));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        objectMapper.writeValue(writer, transports);
     }
 }
