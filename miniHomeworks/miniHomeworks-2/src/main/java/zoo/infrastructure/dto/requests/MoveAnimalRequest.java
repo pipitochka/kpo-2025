3121c414ca9e7b1bcf554cfe8b1eeaf3b52ffd73
pipitochka <aaakulov_3@edu.hse.ru>
2025-04-23 14:56:59 +0300

    changes in movement controller

--- miniHomeworks/miniHomeworks-2/src/main/java/zoo/infrastructure/dto/requests/MoveAnimalRequest.java
+++ miniHomeworks/miniHomeworks-2/src/main/java/zoo/infrastructure/dto/requests/MoveAnimalRequest.java
@@ -1,9 +1,20 @@
 package zoo.infrastructure.dto.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 public class MoveAnimalRequest {
+    @NotNull(message = "Name is required")
+    @Schema(example = "Leo")
+    private String name;
+
+    @NotNull(message = "Enclosure ID is required")
+    @Schema(example = "8a1f1e9a-5d3e-4c8b-9d4a-123456789abc", description = "ID of the animal")
+    private UUID enclosureId;
 }
