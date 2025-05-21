26717de6bba9d9eb88f55d3392b09538fc3f824e
pipitochka <aaakulov_3@edu.hse.ru>
2025-05-22 01:39:58 +0300

    finish car controller tests

--- practice/practise-9/src/test/java/hse/kpo/CarControllerTest.java
+++ practice/practise-9/src/test/java/hse/kpo/CarControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hse.kpo.controllers.car.CarController;
 import hse.kpo.domains.objects.Car;
 import hse.kpo.dto.CarRequest;
+import hse.kpo.dto.CustomerRequest;
 import hse.kpo.interfaces.FacadeIterface;
 import hse.kpo.services.HseCarService;
 import hse.kpo.storages.CarStorage;
@@ -17,12 +18,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BeanPropertyBindingResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
 import org.springframework.http.MediaType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -56,7 +55,7 @@ public class CarControllerTest {
                         .content(json))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.engine.size").value(15))
-                .andExpect(jsonPath("$.engineType").value("PedalEngine(size=15)"));
+                .andExpect(jsonPath("$.engineType").value("PEDAL"));
     }
 
     @Test
@@ -75,4 +74,124 @@ public class CarControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void testFiltredCars() throws Exception {
+        CarRequest carRequest = new CarRequest("PEDAL", 10);
+        String json = objectMapper.writeValueAsString(carRequest);
+
+        mockMvc.perform(post("/api/cars")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated());
+
+        CarRequest carRequest1 = new CarRequest("PEDAL", 13);
+        String json1 = objectMapper.writeValueAsString(carRequest1);
+
+        mockMvc.perform(post("/api/cars")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json1))
+                .andExpect(status().isCreated());
+
+        CarRequest carRequest2 = new CarRequest("HAND", null);
+        String json2 = objectMapper.writeValueAsString(carRequest2);
+
+        mockMvc.perform(post("/api/cars")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json2))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/api/cars")
+                        .param("engineType", "PEDAL"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].engineType").value("PEDAL"))
+                .andExpect(jsonPath("$[1].engineType").value("PEDAL"));
+
+    }
+
+    @Test
+    public void testSellCars() throws Exception {
+        CarRequest carRequest = new CarRequest("PEDAL", 13);
+        String json = objectMapper.writeValueAsString(carRequest);
+
+        mockMvc.perform(post("/api/cars")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated());
+
+        CarRequest carRequest1 = new CarRequest("PEDAL", 9);
+        String json1 = objectMapper.writeValueAsString(carRequest1);
+
+        mockMvc.perform(post("/api/cars")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json1))
+                .andExpect(status().isCreated());
+
+        CustomerRequest customerRequest = new CustomerRequest("Artem", 15, 12, 300);
+        String json3 = objectMapper.writeValueAsString(customerRequest);
+
+        mockMvc.perform(post("/api/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json3))
+                .andExpect(status().isCreated());
+
+        CustomerRequest customerRequest1 = new CustomerRequest("Vanya", 10, 13, 200);
+        String json4 = objectMapper.writeValueAsString(customerRequest1);
+
+        mockMvc.perform(post("/api/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json4))
+                .andExpect(status().isCreated());
+
+
+
+        mockMvc.perform(post("/api/cars/sell"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/customers/Artem"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.car").isNotEmpty());
+
+        mockMvc.perform(get("/api/customers/Vanya"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.car").isNotEmpty());
+    }
+
+    @Test
+    public void updateAndDeleteCar() throws Exception {
+        CarRequest carRequest = new CarRequest("PEDAL", 13);
+        String json = objectMapper.writeValueAsString(carRequest);
+
+        mockMvc.perform(post("/api/cars")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/api/cars"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].engineType").value("PEDAL"));
+
+
+        int vin = facade.getCars().get(0).getVin();
+        CarRequest newCarRequest = new CarRequest("HAND", 13);
+        String newJson = objectMapper.writeValueAsString(newCarRequest);
+
+        mockMvc.perform(put("/api/cars/" + vin)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newJson))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/cars"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].engineType").value("HAND"));
+
+        mockMvc.perform(delete("/api/cars/" + vin))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get("/api/cars"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
 }
